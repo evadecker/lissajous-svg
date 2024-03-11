fbd0906cb5c9f5d1b34ba17d9cb848ed1e88e86a
Eva Decker <itsevadecker@gmail.com>
2024-03-11 00:56:59 -0400

    Update params, add color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,193 +1,211 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { Pane } from 'tweakpane';
-	import { browser } from '$app/environment';
 
-	if (browser) {
-		let params = {
-			angularFrequency: { x: 3, y: 4 },
-			phaseShift: 0,
-			gridCount: 4,
-			gridTickCount: 4,
-			gridTickSize: 2,
-			color: 'orange'
-		};
+	let params = {
+		angularFrequency: { x: 3, y: 4 },
+		phaseShift: Math.PI / 2,
+		strokeWidth: 1,
+		gridCount: 4,
+		gridTickCount: 4,
+		gridTickSize: 2,
+		gridStrokeWidth: 0.5,
+		color: 'orange'
+	};
 
-		const draw = () => {
-			const svg = document.querySelector('.canvas');
+	const draw = () => {
+		const svg = document.querySelector('.canvas');
 
-			// Clear contents
-			if (svg) {
-				while (svg.firstChild) {
-					svg.removeChild(svg.firstChild);
-				}
+		// Clear contents
+		if (svg) {
+			while (svg.firstChild) {
+				svg.removeChild(svg.firstChild);
+			}
+		}
+
+		const drawGrid = (svg: Element, count: number, width: number) => {
+			if (count === 0) return;
+
+			// Major horizontal lines
+			for (let i = 0; i <= count; i++) {
+				const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+				line.setAttribute('x1', '0');
+				line.setAttribute('y1', `${((200 - width) / count) * i + width / 2}`);
+				line.setAttribute('x2', '200');
+				line.setAttribute('y2', `${((200 - width) / count) * i + width / 2}`);
+				line.setAttribute('stroke', `var(--${params.color}-4)`);
+				line.setAttribute('stroke-width', `${width}`);
+				svg.appendChild(line);
 			}
 
-			const drawGrid = (svg: Element, count: number, width: number) => {
-        if (count === 0) return;
-        
-				// Major horizontal lines
-				for (let i = 0; i <= count; i++) {
-					const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-					line.setAttribute('x1', '0');
-					line.setAttribute('y1', `${((200 - width) / count) * i + width / 2}`);
-					line.setAttribute('x2', '200');
-					line.setAttribute('y2', `${((200 - width) / count) * i + width / 2}`);
-					line.setAttribute('stroke', `var(--${params.color}-4)`);
-					line.setAttribute('stroke-width', `${width}`);
-					svg.appendChild(line);
-				}
+			// Major vertical lines
+			for (let i = 0; i <= count; i++) {
+				const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+				line.setAttribute('x1', `${((200 - width) / count) * i + width / 2}`);
+				line.setAttribute('y1', `${width}`);
+				line.setAttribute('x2', `${((200 - width) / count) * i + width / 2}`);
+				line.setAttribute('y2', `${200 - width}`);
+				line.setAttribute('stroke', `var(--${params.color}-4)`);
+				line.setAttribute('stroke-width', `${width}`);
+				svg.appendChild(line);
+			}
 
-				// Major vertical lines
-				for (let i = 0; i <= count; i++) {
-					const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-					line.setAttribute('x1', `${((200 - width) / count) * i + width / 2}`);
-					line.setAttribute('y1', `${width}`);
-					line.setAttribute('x2', `${((200 - width) / count) * i + width / 2}`);
-					line.setAttribute('y2', `${200 - width}`);
-					line.setAttribute('stroke', `var(--${params.color}-4)`);
-					line.setAttribute('stroke-width', '0.5');
-					svg.appendChild(line);
-				}
+			// Minor horizontal tickmarks
+			for (let i = 0; i <= count * (params.gridTickCount + 1); i++) {
+				const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+				line.setAttribute('x1', `${200 / 2 - params.gridTickSize}`);
+				line.setAttribute(
+					'y1',
+					`${((200 - width) / count / (params.gridTickCount + 1)) * i + width / 2}`
+				);
+				line.setAttribute('x2', `${200 / 2 + params.gridTickSize}`);
+				line.setAttribute(
+					'y2',
+					`${((200 - width) / count / (params.gridTickCount + 1)) * i + width / 2}`
+				);
+				line.setAttribute('stroke', `var(--${params.color}-4)`);
+				line.setAttribute('stroke-width', `${width}`);
+				svg.appendChild(line);
+			}
 
-				// Minor horizontal tickmarks
-				for (let i = 0; i <= count * (params.gridTickCount + 1); i++) {
-					const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-					line.setAttribute('x1', `${200 / 2 - params.gridTickSize}`);
-					line.setAttribute(
-						'y1',
-						`${((200 - width) / count / (params.gridTickCount + 1)) * i + width / 2}`
-					);
-					line.setAttribute('x2', `${200 / 2 + params.gridTickSize}`);
-					line.setAttribute(
-						'y2',
-						`${((200 - width) / count / (params.gridTickCount + 1)) * i + width / 2}`
-					);
-					line.setAttribute('stroke', `var(--${params.color}-4)`);
-					line.setAttribute('stroke-width', `${width}`);
-					svg.appendChild(line);
-				}
-
-				// Minor vertical tickmarks
-				for (let i = 0; i <= count * (params.gridTickCount + 1); i++) {
-					const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-					line.setAttribute(
-						'x1',
-						`${((200 - width) / count / (params.gridTickCount + 1)) * i + width / 2}`
-					);
-					line.setAttribute('y1', `${200 / 2 - params.gridTickSize}`);
-					line.setAttribute(
-						'x2',
-						`${((200 - width) / count / (params.gridTickCount + 1)) * i + width / 2}`
-					);
-					line.setAttribute('y2', `${200 / 2 + params.gridTickSize}`);
-					line.setAttribute('stroke', `var(--${params.color}-4)`);
-					line.setAttribute('stroke-width', '0.5');
-					svg.appendChild(line);
-				}
-			};
-
-			const drawLissajous = (svg: Element, a: number, b: number) => {
-				const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-				const points = [];
-
-				for (let t = 0; t <= Math.PI * 2 + 0.1; t += 0.01) {
-					const x = Math.sin(a * t + params.phaseShift);
-					const y = Math.sin(b * t);
-					points.push(`${x * 80 + 100},${y * 80 + 100}`);
-				}
-
-				path.setAttribute('d', `M${points.join('L')}`);
-				path.setAttribute('fill', 'none');
-				path.setAttribute('stroke', `var(--${params.color}-9)`);
-				path.setAttribute('stroke-width', '1');
-				path.setAttribute('stroke-linecap', 'square');
-				path.setAttribute('stroke-linejoin', 'round');
-				svg.appendChild(path);
-			};
-
-			if (svg) {
-				drawGrid(svg, params.gridCount, 0.5);
-				drawLissajous(svg, params.angularFrequency.x, params.angularFrequency.y);
+			// Minor vertical tickmarks
+			for (let i = 0; i <= count * (params.gridTickCount + 1); i++) {
+				const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+				line.setAttribute(
+					'x1',
+					`${((200 - width) / count / (params.gridTickCount + 1)) * i + width / 2}`
+				);
+				line.setAttribute('y1', `${200 / 2 - params.gridTickSize}`);
+				line.setAttribute(
+					'x2',
+					`${((200 - width) / count / (params.gridTickCount + 1)) * i + width / 2}`
+				);
+				line.setAttribute('y2', `${200 / 2 + params.gridTickSize}`);
+				line.setAttribute('stroke', `var(--${params.color}-4)`);
+				line.setAttribute('stroke-width', `${width}`);
+				svg.appendChild(line);
 			}
 		};
 
-		onMount(() => {
-			const pane = new Pane();
+		const drawLissajous = (svg: Element, a: number, b: number) => {
+			const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+			const points = [];
 
-      const equation = pane.addFolder({
-        title: 'Lissajous Curve',
-      });
+			for (let t = 0; t <= Math.PI * 2 + 0.1; t += 0.01) {
+				const x = Math.sin(a * t + params.phaseShift);
+				const y = Math.sin(b * t);
+				points.push(`${x * 80 + 100},${y * 80 + 100}`);
+			}
 
-			equation.addBinding(params, 'angularFrequency', {
-				label: 'frequency',
-				picker: 'inline',
-				expanded: true,
-				x: {
-					min: -16,
-					max: 16,
-					step: 1
-				},
-				y: {
-					min: -16,
-					max: 16,
-					step: 1
-				}
-			});
+			path.setAttribute('d', `M${points.join('L')}`);
+			path.setAttribute('fill', 'none');
+			path.setAttribute('stroke', `var(--${params.color}-9)`);
+			path.setAttribute('stroke-width', `${params.strokeWidth}`);
+			path.setAttribute('stroke-linecap', 'square');
+			path.setAttribute('stroke-linejoin', 'round');
+			svg.appendChild(path);
+		};
 
-			equation.addBinding(params, 'phaseShift', {
-				label: 'phase shift',
-				min: 0,
-				max: Math.PI,
-				step: 0.01
-			});
+		if (svg) {
+			drawGrid(svg, params.gridCount, params.gridStrokeWidth);
+			drawLissajous(svg, params.angularFrequency.x, params.angularFrequency.y);
+		}
+	};
 
-      const grid = pane.addFolder({
-        title: 'Grid',
-      });
+	const setupPane = () => {
+		const pane = new Pane();
 
-			grid.addBinding(params, 'gridCount', {
-				label: 'gridlines',
-				min: 0,
-				max: 12,
-				step: 2
-			});
+		const equation = pane.addFolder({
+			title: 'Lissajous Curve'
+		});
 
-			grid.addBinding(params, 'gridTickCount', {
-				label: 'tickmarks',
-				min: 0,
-				max: 8,
-				step: 1,
-			});
+		equation.addBinding(params, 'angularFrequency', {
+			label: 'frequency',
+			picker: 'inline',
+			expanded: true,
+			x: {
+				min: -16,
+				max: 16,
+				step: 1
+			},
+			y: {
+				min: -16,
+				max: 16,
+				step: 1
+			}
+		});
 
-			grid.addBinding(params, 'gridTickSize', {
-				label: 'tick size',
-				min: 1,
-				max: 3,
-				step: 0.1
-			});
+		equation.addBinding(params, 'phaseShift', {
+			label: 'phase',
+			min: 0,
+			max: Math.PI,
+			step: 0.01,
+		});
 
-      const style = pane.addFolder({
-        title: 'Style',
-      });
+		equation.addBinding(params, 'strokeWidth', {
+			label: 'stroke',
+			min: 0.5,
+			max: 3,
+			step: 0.1
+		});
 
-			style.addBinding(params, 'color', {
-				options: {
-					red: 'red',
-					orange: 'orange',
-					yellow: 'amber',
-					teal: 'teal',
-					violet: 'violet'
-				}
-			});
+		const grid = pane.addFolder({
+			title: 'Grid'
+		});
 
-			pane.on('change', () => {
-				draw();
-			});
+		grid.addBinding(params, 'gridCount', {
+			label: 'gridlines',
+			min: 0,
+			max: 12,
+			step: 2
+		});
 
+		grid.addBinding(params, 'gridTickCount', {
+			label: 'tickmarks',
+			min: 0,
+			max: 8,
+			step: 1
+		});
+
+		grid.addBinding(params, 'gridTickSize', {
+			label: 'tick size',
+			min: 1,
+			max: 3,
+			step: 0.1
+		});
+
+		grid.addBinding(params, 'gridStrokeWidth', {
+			label: 'stroke',
+			min: 0.2,
+			max: 1,
+			step: 0.1
+		});
+
+		const style = pane.addFolder({
+			title: 'Style'
+		});
+
+		style.addBinding(params, 'color', {
+			options: {
+				red: 'red',
+				orange: 'orange',
+				yellow: 'amber',
+        lime: 'lime',
+				teal: 'teal',
+				violet: 'violet',
+        pink: 'pink'
+			}
+		});
+
+		pane.on('change', () => {
 			draw();
 		});
-	}
+	};
+
+	onMount(() => {
+		setupPane();
+		draw();
+	});
 </script>
 
 <svelte:head>
